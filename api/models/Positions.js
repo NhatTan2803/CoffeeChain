38323926728443a223dab68e065b971cdf15a0c8
Nhat Tan <nhattan2803@gmail.com>
2018-05-26 20:17:14 +0700

    done model position

---
+++ api/models/Positions.js
@@ -0,0 +1,21 @@
+/**
+ * Positions.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    position_shop_id: {
+      type: 'string',
+    },
+    position_name: {
+      type: 'string',
+
+    },
+  },
+
+};
+
