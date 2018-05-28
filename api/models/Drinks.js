7d1b045032ef6bceb7211801b21a61240ae8584a
ThongLV <thonglevan131@gmail.com>
2018-05-28 17:18:02 +0700

    Add Drink model

---
+++ api/models/Drinks.js
@@ -0,0 +1,50 @@
+/**
+ * Drinks.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    drink_id: {
+      type: 'number',
+      required: true
+    },
+
+    drink_name: {
+      type: 'string',
+      required: true
+    },
+
+    drink_price: {
+      type: 'number',
+      columnType: 'decimal'
+    },
+
+    drink_avatar: {
+      type: 'string'
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    drink_shop: {
+      model: 'Shops',
+      columnName: 'drink_shop_id',
+      required: true
+    }
+  },
+
+};
+
