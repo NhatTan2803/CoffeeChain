d5e3b66f30581763b728a1169d88a52457df3f98
Nhat Tan <nhattan2803@gmail.com>
2018-06-05 14:04:21 +0700

    add systemcontroller

---
+++ api/controllers/system/SystemController.js
@@ -0,0 +1,70 @@
+/**
+ * SystemController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+  inputs: {
+    name: {
+      description: 'The name of company',
+      type: 'string',
+      required: true
+    },
+    address: {
+      description: 'Address of company',
+      type: 'string'
+    }
+  },
+
+  exits: {
+    success: {
+      responseType: ''
+    },
+
+  },
+  /**
+   * `SystemController.create()`
+   */
+  create: async function (inputs, exits) {
+    var system = await system.create({
+      name: inputs.name,
+      address: inputs.address
+    }).fetch();
+    return exits.success();
+  },
+
+  /**
+   * `SystemController.getList()`
+   */
+  getList: async function (inputs, exits) {
+    try {
+      const results = await System.find()
+        .populate('shops')
+        .populate('points')
+        .fetch();
+      return this.exits.success();
+    } catch (error) {
+      return this.exits.error(error);
+    }
+  },
+
+  /**
+   * `SystemController.getSystemInfo()`
+   */
+  getSystemInfo: async function (inputs, exits) {
+    try {
+      const result = await System.findOne({
+        id: req.param('id')
+      }).populate('shops')
+        .populate('points')
+        .fetch();
+    } catch (error) {
+      return this.exits.errror(error);
+    }
+  }
+
+};
+
