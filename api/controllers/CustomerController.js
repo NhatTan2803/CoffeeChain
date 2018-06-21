6aebc651a1b9b3f63ee8166f81a39ec489a0bf4a
Nhat Tan <nhattan2803@gmail.com>
2018-06-21 13:57:26 +0700

    init Cus controller and action show view

---
+++ api/controllers/CustomerController.js
@@ -0,0 +1,26 @@
+/**
+ * CustomerController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+    showLoginCus: async function (req, res) {
+        return res.view('./pages/customer/login-customer', { layout: 'layouts/layout-intro' });
+    },
+    showSignUpCus: async function (req, res) {
+        return res.view('./pages/customer/signup-customer', { layout: 'layouts/layout-intro' })
+    },
+    showMainCus: async function (req,res) {
+        return res.view('./pages/customer/main-customer', { layout:'layouts/layout-intro'})
+    },
+    showProfileCus: async function (req,res) {
+        return res.view('./pages/customer/profile-customer', { layout: 'layouts/layout-intro' })
+    },
+    showRateCus: async function (req, res) {
+        return res.view('./pages/customer/profile-customer', { layout: 'layouts/layout-intro' })
+    },
+
+};
+
