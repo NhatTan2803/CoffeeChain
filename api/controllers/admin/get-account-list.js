0776c9c4e57e85a6887654ed1f42d0ed7a9ac0b4
ThongLV <thonglevan131@gmail.com>
2018-06-26 11:39:12 +0700

    updated the functionality of only view boss account. If not found, view display 'No records found'

--- api/controllers/admin/get-account-list.js
+++ api/controllers/admin/get-account-list.js
@@ -18,23 +18,24 @@ module.exports = {
         viewTemplatePath: './pages/coffee/admin/listAccount'
     },
     notFound: {
-      // description: 'The data of accounts are empty',
-      // respondType: 'notFound'
+      description: 'The data of accounts are empty',
+      respondType: 'notFound'
     }
   },
 
 
   fn: async function (inputs, exits) {
     await User.find({
-      where: { id: this.req.cookies.id },
+      where: { 
+        id: this.req.cookies.id, 
+        permission: 'boss'
+      },
       select: ['name', 'email', 'shops','active', 'phone']
     })
       .populate('shops')
       .exec(function (err, account) {
+        sails.log("account : " + {account});
         if (err) return exits.error(err);
-        if (account.length == 0) {
-          return exits.notFound();
-        }
         return exits.success({account:account});
       })
   }
