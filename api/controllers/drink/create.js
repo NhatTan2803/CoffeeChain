cf750633ce6c94072031064955806cc34b7a46e4
ThongLV <thonglevan131@gmail.com>
2018-06-29 16:34:26 +0700

    modified drink/create.js

--- api/controllers/drink/create.js
+++ api/controllers/drink/create.js
@@ -6,44 +6,69 @@ module.exports = {
 
   description: 'Create drink.',
 
+  files: ['photo'],
 
   inputs: {
+    photo: {
+      example: '===',
+      required: true
+    },
     name: {
-      type: 'string'
+      type: 'string',
+      required: true
     },
     price: {
       type: 'number',
       columnType: 'decimal'
     },
-    avatar: {
-      type: 'string'
+    shops: {
+      type: 'number'
     },
-    shop: {
-      type: 'number',
-      description: 'ID of shop to look up'
-    }
   },
-  exits: {
-    success: {
 
-    },
-    error: {
-      description: 'No shop is created'
-    }
+
+  exits: {
+
   },
+
+
   fn: async function (inputs, exits) {
-    await Drink.create({
-      name: inputs.name,
-      price: inputs.price,
-      avatar: inputs.avatar,
-      shops: inputs.shop
-    }).fetch()
-      .exec(function (err, created) {
-        if(err)
-        return exits.error(err)
-        else
-        return exits.success(created)
-      })
+    try {
+      var createdDrink = await Drink.create({
+        name: inputs.name,
+        price: inputs.price,
+        avatar: inputs.avatar,
+        shops: inputs.shops
+      }).fetch();
+    } catch (error) {
+      return exits.error(error);
+    }
+    sails.log("SHOPSID : " + createdDrink.shops);
+    inputs.photo.upload({
+      dirname: require('path').resolve(sails.config.appPath, 'assets/images/drinks'),
+      saveAs: function (__newFileStream, next) {
+        return next(undefined, createdDrink.id + '-drink.jpg');
+      },
+      maxBytes: 1000000
+    }, (uploadingError, uploadedFiles) => {
+      if (uploadingError) {
+        return exits.error(uploadingError);
+      }
+      if (uploadedFiles.length > 0) {
+        Drink.update({
+            id: createdDrink.id
+          }, {
+            avatar: require('path').basename(uploadedFiles[0].fd)
+          })
+          .exec((updatedError, updatedDrink) => {
+            if (updatedError){
+              return exits.error(updatedError);
+            }
+            return exits.success(updatedDrink);
+          })
+      }
+    });
+
   }
 
 
