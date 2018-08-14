ad06b5495706e74e8b3650c2cf4c7dcaf7555170
Nhat Tan <nhattan2803@gmail.com>
2018-08-14 10:13:58 +0700

    change folder updload and fix link save

--- api/controllers/admin/create-shop.js
+++ api/controllers/admin/create-shop.js
@@ -75,7 +75,7 @@ module.exports = {
 
     const files = await new Promise((resolve, reject) => {
       inputs.photo.upload({
-        dirname: path.resolve(sails.config.appPath, 'uploads'),
+        dirname: path.resolve(sails.config.appPath, 'uploads/shops'),
         saveAs: shop.id + '-shop-avatar.png',
         maxBytes: 1000000
       }, (error, result) => {
