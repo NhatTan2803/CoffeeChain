c81b5d12382d1c8d67d7294edfb1f71e21fe1bb4
Nhat Tan <nhattan2803@gmail.com>
2018-08-22 10:23:38 +0700

    config react route

---
+++ assets/jsintro/React/Order-customer.jsx
@@ -0,0 +1,72 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class OrderCustomer extends React.Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-6 product-bottom">
+                    <div className="categories animated wow fadeInUp animated" data-wow-delay=".5s">
+                        <div style="border: 1px solid rgb(238, 238, 238); visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Username</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 animated wow fadeInRight" data-wow-delay=".5s">
+                    <div className="categories animated wow fadeInUp animated" data-wow-delay=".5s">
+                        <div style="border: 1px solid rgb(238, 238, 238); visibility: visible; animation-delay: 0.5s; animation-name: fadeInUp;">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th></th>
+                                        <th>Last Name</th>
+                                        <th>Username</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>Larry</td>
+                                        <td>the Bird</td>
+                                        <td>@twitter</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<OrderCustomer />, document.querySelector('#Order-customer'));
--- tasks/config/browserify.js
+++ tasks/config/browserify.js
@@ -2,10 +2,12 @@ module.exports = function (grunt) {
     grunt.config.set('browserify', {
         client: {
             files: {
-                '.tmp/public/js/React/bsell.js': ['.tmp/public/js/React/sell.js'],
-                '.tmp/public/js/React/bposition.js': ['.tmp/public/js/React/position.js'],
-                '.tmp/public/js/React/bsystem.js': ['.tmp/public/js/React/system.js'],
-                '.tmp/public/js/React/bshopCreate.js': ['.tmp/public/js/React/shopCreate.js']
+                '.tmp/public/js/React/sell.js': ['.tmp/public/js/React/sell.js'],
+                '.tmp/public/js/React/position.js': ['.tmp/public/js/React/position.js'],
+                '.tmp/public/js/React/system.js': ['.tmp/public/js/React/system.js'],
+                '.tmp/public/js/React/shopCreate.js': ['.tmp/public/js/React/shopCreate.js'],
+                '.tmp/public/jsintro/React/Order-customer.js': ['.tmp/public/jsintro/React/Order-customer.js']
+
             }
         },
     });
